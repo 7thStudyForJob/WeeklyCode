9d6d8daa736eedbe65b17a6f800d50ad9a07dc63
Donghee <dongh94@naver.com>
2023-04-28 17:22:14 +0900

    4월 막주

---
+++ 김동희/백준/백트래킹/N과M2/N과M2.py
@@ -0,0 +1,19 @@
+import sys; sys.stdin=open("N과M2.txt")
+n, m = map(int, sys.stdin.readline().split())
+
+result = [0] * m
+check = [False] *(n+1)
+
+def dfs(dept, cut):
+    if dept == m:
+        print(*result)
+        return
+
+    for i in range(1, n+1):
+        if not check[i] and cut < i:
+            check[i] = True
+            result[dept] = i
+            dfs(dept + 1, i)
+            check[i] = False
+
+dfs(0, 0)
